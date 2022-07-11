b4b98064cf6da19eef0382b447432976eeb9ad2c
n1ghtx0w1 <rhead13@wgu.edu>
2022-07-11 00:39:11 -0500

    finished blog posts

--- sanity-app-build/pages/post/[slug].tsx
+++ sanity-app-build/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import PortableText from 'react-portable-text';
 import Header from '../../components/Header';
 import { sanityClient, urlFor } from '../../sanity';
 import { Post } from '../../typings';
@@ -11,7 +12,12 @@ function Post({ post }: Props) {
   return (
     <main>
         <Header />
-        <img className='w-full h-40 object-cover' src={urlFor(post.mainImage).url()!} alt="Image Text" />
+
+        <img 
+        className='w-full h-40 object-cover' 
+        src={urlFor(post.mainImage).url()!} 
+        alt="Image Text" />
+
         <article className='max-w-3xl mx-auto p-5'>
             <h1 className='text-3xl mt-10 mb-3'>
                 {post.title}
@@ -25,8 +31,34 @@ function Post({ post }: Props) {
                 Blog post by <span className='text-blue-600 font-bold'> {post.author.name} </span> - Published at {new Date(post._createdAt).toLocaleString()}
             </p>
             </div>
-            
+            <div className='mt-10'>
+                <PortableText
+                className=''
+                dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                content={post.body}
+                serializers={
+                    {
+                    h1: (props: any) => (
+                        <h1 className='text-2xl font-bold my-5' {...props} />
+                    ),
+                    h2: (props: any) => (
+                        <h2 className='text-xl font-bold my-5' {...props} />
+                    ),
+                    li: ({ children }: any) => (
+                        <li className='ml-4 list-disc'>{children}</li>
+                    ),
+                    link: ({ href, children }: any) => (
+                        <a href={href} className="text-blue-500 hover:underline">
+                            {children}
+                        </a>
+                    ),
+                }
+            }
+                />
+            </div>
         </article>
+        <hr className='max-w-lg my-5 mx-auto border border-blue-500' />
     </main>
   )
 };
